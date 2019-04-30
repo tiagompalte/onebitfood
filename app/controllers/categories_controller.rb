fc90ab234cf03bf195b6cb3d10839d93d1beab62
Tiago Mariani Palte <tiagompalte@hotmail.com>
2019-04-29 21:27:32 -0300

    Initial MVP API

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Categorias
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all.order(:title)
+    render json: @categories
+  end
+end
---
+++ app/serializers/product_category_serializer.rb
@@ -0,0 +1,4 @@
+class ProductCategorySerializer < ActiveModel::Serializer
+  attributes :id, :title 
+  has_many :products
+end
